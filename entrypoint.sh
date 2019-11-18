651a01fe1c6c681e2f630fb32ef91ec71f75bbd4
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-11-17 23:10:48 -0500

    correccion de entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export NOVEDADES_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/novedades_crud/db/username --output text --query Parameter.Value)"
-  export NOVEDADES_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/novedades_crud/db/password --output text --query Parameter.Value)"
+  export MINUTAS_ARGO_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/minutas_crud/db/username --output text --query Parameter.Value)"
+  export MINUTAS_ARGO_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/minutas_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
